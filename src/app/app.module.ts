8e7ae8c257c558144b3b100bb8712b63d46e60f6
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-12 11:09:45 +0200

    Create index component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './component/card/card.component';
 import { AssetsComponent } from './dev/assets/assets.component';
 import {RoutingModule} from "./routing.module";
+import {IndexComponent} from "./component/index/index.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    AssetsComponent
+    AssetsComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
