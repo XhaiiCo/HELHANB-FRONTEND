44e4e56a9a567cf1910ceb1db30603b168aa7766
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-12 11:07:56 +0200

    Create router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './component/card/card.component';
 import { AssetsComponent } from './dev/assets/assets.component';
+import {RoutingModule} from "./routing.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AssetsComponent } from './dev/assets/assets.component';
     AssetsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AssetsComponent} from "./dev/assets/assets.component";
+import {RouterModule, Routes} from "@angular/router";
 
-
+const routes: Routes = [
+  {path: 'assets', component: AssetsComponent  }
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class RoutingModule { }
