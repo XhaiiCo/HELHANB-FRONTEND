554241b02faaba80c8eb930eb767519cf5700abe
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-12 19:31:16 +0200

    Add carousel

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselModule} from "ngx-bootstrap/carousel";
 
 @Component({
   selector: 'app-card',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  nbAds: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+  images: any  = [
+    "../assets/img/maison1.1.webp",
+    "../assets/img/maison1.2.webp",
+    "../assets/img/maison1.3.webp",
+  ]
   constructor() { }
 
   ngOnInit(): void {
