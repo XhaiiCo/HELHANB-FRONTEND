dbdbf1db20d826561d695d039e890fb0078e63c0
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-12 11:12:23 +0200

    Add a link to the assets page

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AssetsComponent} from "./dev/assets/assets.component";
 import {RouterModule, Routes} from "@angular/router";
+import {IndexComponent} from "./component/index/index.component";
 
 const routes: Routes = [
-  {path: 'assets', component: AssetsComponent  }
+  {path: 'assets', component: AssetsComponent  },
+  {path: '**', component: IndexComponent}
 ]
 
 @NgModule({
