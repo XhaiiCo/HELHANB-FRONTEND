a7e45783908668ee763b4d87662b4a26695baba8
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-18 21:53:23 +0200

    Removed unused field in card component

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -6,9 +6,6 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
-  nbAds: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
   @Input() images: string[]  = []
   constructor() { }
 
