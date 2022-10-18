abd1ed823333da02942b28389d5eac79e778c744
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-10-18 21:50:14 +0200

    Refacto: set carousel into card

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -9,12 +9,7 @@ export class CardComponent implements OnInit {
 
   nbAds: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-  images: string[]  = [
-    "../assets/img/maison1.1.webp",
-    "../assets/img/maison1.2.webp",
-    "../assets/img/maison1.3.webp",
-    "https://www.zooplus.be/magazine/wp-content/uploads/2019/06/comprendre-le-langage-des-chats.jpg"
-  ]
+  @Input() images: string[]  = []
   constructor() { }
 
   ngOnInit(): void {
