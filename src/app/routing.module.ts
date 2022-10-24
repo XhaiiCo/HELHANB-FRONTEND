06f6d73fad6bec877f166ba006867ec0eb047083
Leclercq David <davidleclercq2002@gmail.com>
2022-10-24 10:03:19 +0200

    Add the registration route

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -8,10 +8,12 @@ import {CropperComponent} from "./component/cropper/cropper.component";
 import {NotFoundComponent} from "./views/not-found/not-found.component";
 import {HomepageComponent} from "./views/homepage/homepage.component";
 import {LoginComponent} from "./views/login/login.component";
+import {RegistrationComponent} from "./views/registration/registration.component";
 
 const routes: Routes = [
   {path: '', component: HomepageComponent   },
   {path: 'connexion', component: LoginComponent},
+  {path: 'inscription', component: RegistrationComponent},
   {path: 'dev', component: IndexComponent},//Remove
   {path: 'assets', component: AssetsComponent  },//Remove
   {path: 'card', component: CardComponent},//Remove
