4c5af434759654d2a48a23622ae14ad8e06e0c32
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2022-11-03 18:20:13 +0100

    Created divs container and title container value

--- src/app/views/renting/renting.component.ts
+++ src/app/views/renting/renting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-renting',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./renting.component.scss']
 })
 export class RentingComponent implements OnInit {
+
+  @Input() daily_price: number = 149.99;
+  @Input() ad_name: string = "Chalet au bord du lac";
+  @Input() owner_name: string = "François";
+  @Input() nb_max_tenant: number = 4;
+  @Input() nb_bedroom: number = 2;
+  @Input() nb_bed: number = 3;
+  str_nb_max_tenant: string = this.nb_max_tenant > 1 ? "voyageurs" : "voyageur";
+  str_nb_bedroom: string = this.nb_bedroom > 1 ? "chambres" : "chambre";
+  str_nb_bed: string = this.nb_bed > 1 ? "lits" : "lit";
 
   constructor() { }
 
