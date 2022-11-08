ba9d7fec06645ece689ab237c93acc8c368e4e6a
Leclercq David <davidleclercq2002@gmail.com>
2022-11-08 14:53:36 +0100

    Remove toast notification after 5 seconds

--- src/app/services/toast-notification.service.ts
+++ src/app/services/toast-notification.service.ts
@@ -9,31 +9,31 @@ export class ToastNotificationService {
   notifications: ToastNotification[] = [] ;
   private readonly possibleTypes: string[] = ["success", "error", "info"];
   private readonly defaultType: string = "info" ;
+  private id: number = 1 ;
 
   constructor() { }
 
   add(content: string, type: string): void{
     if(!this.possibleTypes.includes(type)) type = this.defaultType ;
     let newNotification: ToastNotification = {
-      id: this.generateNewId(),
+      id: ++this.id,
       content: content,
       type: type,
       isRemove: false,
     }
 
     this.notifications.push(newNotification) ;
+
+    setTimeout(() => this.remove(newNotification.id), 5000) ;
   }
 
   remove(id: number){
     let notif = this.notifications.find(value => value.id == id) ;
     if(notif)
       notif.isRemove = true ;
-    //this.notifications = this.notifications.filter(value => value.id != id) ;
-  }
-  private generateNewId(): number{
-    if(this.notifications.length)
-      return this.notifications.map(value => value.id).reduce((a, b) => Math.max(a, b)) + 1 ;
 
-    return 0 ;
+    setTimeout(() => {
+      this.notifications = this.notifications.filter(value => value.id != id) ;
+    }, 1000) ;
   }
 }
