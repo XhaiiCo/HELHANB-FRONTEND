4c3134fb792b54d4e0a2efb1ebc0df59536c5a68
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2022-11-17 12:51:40 +0100

    Change variables name

--- src/app/views/renting/renting.component.ts
+++ src/app/views/renting/renting.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {environment} from "../../../environments/environment";
 
 const dayDif = (date1: Date, date2: Date) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
 
@@ -10,14 +12,16 @@ const dayDif = (date1: Date, date2: Date) => Math.ceil(Math.abs(date1.getTime() 
 export class RentingComponent implements OnInit {
 
   @Input() daily_price: number = 149.99;
-  @Input() ad_name: string = "Chalet au bord du lac";
-  @Input() owner_name: string = "François";
+  @Input() ad_name: string = "Chat laid au bord du lac";
+  @Input() host_name: string = "François";
+  @Input() host_pp: string = "";
+  profilePictureBaseUri: string  = environment.pictureUrl;
 
   @Input() nb_max_tenant: number = 4;
-  @Input() nb_bedroom: number = 2;
-  @Input() nb_bed: number = 3;
   str_nb_max_tenant: string = this.nb_max_tenant > 1 ? "voyageurs" : "voyageur";
+  @Input() nb_bedroom: number = 2;
   str_nb_bedroom: string = this.nb_bedroom > 1 ? "chambres" : "chambre";
+  @Input() nb_bed: number = 3;
   str_nb_bed: string = this.nb_bed > 1 ? "lits" : "lit";
 
   beginDate!: Date;
@@ -32,7 +36,6 @@ export class RentingComponent implements OnInit {
   ]
 
   displayAllFeatures: boolean = false;
-
   @Input() renting_features: string[] =
     [
       "Wifi",
@@ -46,7 +49,7 @@ export class RentingComponent implements OnInit {
       "Chauffage"
     ]
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -63,7 +66,7 @@ export class RentingComponent implements OnInit {
   }
 
   submit() {
-    console.log("test");
+
   }
 
   showAllFeatures() {
