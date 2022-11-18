496824b4af25526a262a4ec274185121aa849d58
Leclercq David <davidleclercq2002@gmail.com>
2022-11-18 15:18:30 +0100

    Make reservations list for user

--- src/app/views/my-reservation/my-reservation.component.ts
+++ src/app/views/my-reservation/my-reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-reservation',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyReservationComponent implements OnInit {
 
-  constructor() { }
+  reservationsList: any[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+
+    //Create fake reservation
+    this.reservationsList.push(
+      {
+        name: "Saint-Siffret",
+        destination: "Tournai",
+        host_name: "François",
+        beginDate: "1 juil, 2021",
+        endDate: "9 juil 2021",
+        reservationPicture: "https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/dfe9fd1e-a010-43c9-b546-0bbc7d59f7f3.jpeg?im_w=1200",
+      },
+      {
+        name: "chat laid au bord du lac",
+        destination: "Tournai",
+        host_name: "Hôte: François",
+        beginDate: "1 juil, 2021",
+        endDate: "9 juil 2021",
+        reservationPicture: "https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/53b475a3-104f-462e-8faf-85a7bcd1f13b.jpeg?im_w=720",
+      },
+      {
+        name: "chat laid au bord du lac",
+        destination: "Tournai",
+        host_name: "François",
+        beginDate: "1 juil, 2021",
+        endDate: "9 juil 2021",
+        reservationPicture:
+          "https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/56fa6c39-d99f-49d9-91de-4db146a55db9.jpeg?im_w=1200",
+
+      }
+    );
   }
 
 }
