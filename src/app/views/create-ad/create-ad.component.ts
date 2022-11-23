06cfb9d1adcbba3d98811e3f2a701049e9d05fa8
JeremSSJ <jeremssj7@gmail.com>
2022-11-23 11:38:10 +0100

    formulaire un peu refactor

--- src/app/views/create-ad/create-ad.component.ts
+++ src/app/views/create-ad/create-ad.component.ts
@@ -10,25 +10,25 @@ export class CreateAdComponent implements OnInit {
 
   submitBtnValue: string = "Suivant";
   step : number = 0;
-  stepsName : string[] = ["step1", "step2", "step3", "step4"]
+  stepsName : string[] = ["step0", "step1", "step2", "step3"]
 
   adCreateForm = new FormGroup({
 
-    step1: new FormGroup({
+    step0: new FormGroup({
       name: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
     }),
 
-    step2: new FormGroup({
+    step1: new FormGroup({
       street: new FormControl('', Validators.required),
       city: new FormControl('', Validators.required),
       postalCode: new FormControl('', Validators.required),
       country: new FormControl('', Validators.required)
     }),
 
-    step3: new FormGroup({
-      pricePerNight: new FormControl(0, [Validators.required, Validators.pattern(/^\d+(,|.\d{1,2})?$/)]),
-      nbOfPersons: new FormControl(0, [Validators.required, Validators.pattern(/^\d*[1-9]\d*$/)])
+    step2: new FormGroup({
+      pricePerNight: new FormControl('', [Validators.required, Validators.pattern(/^\d+(,|.\d{1,2})?$/)]),
+      nbOfPersons: new FormControl('', [Validators.required, Validators.pattern(/^\d*[1-9]\d*$/)])
     })
 
   })
@@ -60,46 +60,10 @@ export class CreateAdComponent implements OnInit {
     this.submitBtnValue = this.step < 4 ? "Suivant" : "Valider";
   }
 
-  //step1
-  get inputName():AbstractControl | null | undefined
+  isInvalid(subFormName: string, controlName: string)
   {
-    return this.adCreateForm.get("step1")?.get("name");
-  }
+      var control = this.adCreateForm.get(subFormName)?.get(controlName);
 
-  get inputDescription():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step1")?.get("description");
-  }
-
-  //step2
-  get inputStreet():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step2")?.get("street");
-  }
-
-  get inputCity():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step2")?.get("city");
-  }
-
-  get inputPostalCode():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step2")?.get("postalCode");
-  }
-
-  get inputCountry():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step2")?.get("country");
-  }
-
-  //step3
-  get inputPricePerNight():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step3")?.get("pricePerNight");
-  }
-
-  get inputNbOfPersons():AbstractControl | null | undefined
-  {
-    return this.adCreateForm.get("step3")?.get("nbOfPersons");
+      return control?.dirty && control?.invalid;
   }
 }
