220c4ce4d5b904fd269e95cd84533125d4205e95
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2022-11-28 12:51:16 +0100

    Add date availability

--- src/app/component/date-picker/date-picker.component.ts
+++ src/app/component/date-picker/date-picker.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import 'moment/locale/fr';
+import {ToastNotificationService} from "../../services/toast-notification.service";
 
 const now = new Date();
 
@@ -44,13 +45,54 @@ export class DatePickerComponent implements OnInit {
     return !this.notAvailableDates.find(x => new Date(x).getTime() == time);
   }
 
-  constructor() {
-  }
+  constructor(private _ToastNotificationService: ToastNotificationService) {}
 
   ngOnInit(): void {
   }
 
-  dateChange() {
-    this.emitDateChange.next(this.range);
+  startDateChange(e: any) { // Changer max pour les dates indispos
+    /*const date_tmp = e.value;
+
+    if (!date_tmp) return;
+
+    const date = new Date(date_tmp).getTime();
+    if (date == this.max.getTime()) {
+      this.dateDPError("Cette date n'est pas disponible");
+      return;
+    }
+
+    if (this.notAvailableDates.length == 0) return;
+
+    let max_tmp = this.notAvailableDates.filter(x => new Date(x).getTime() > date);
+
+    if (max_tmp.length != 0) {
+      max_tmp.sort((a, b) => a.getTime() - b.getTime());
+      //if (date == max_tmp[0].getTime() - (1000 * 60 * 60 * 24))
+        //this.dateDPError("Cette date n'est pas disponible");
+    }*/
+  }
+
+  private dateDPError(sent: string) {
+    // Fermer le dp + reset la valeur d'input
+    this.range.reset();
+    this._ToastNotificationService.add(`${sent}`, "error");
+  }
+
+  endDateChange(e: any) {
+    const date_tmp = e.value;
+
+    if (!date_tmp) return;
+
+    const date = new Date(date_tmp).getTime();
+
+    if (this.notAvailableDates.length == 0) return;
+
+    if (this.range.controls.start.value == null) return;
+    // @ts-ignore
+    let max_tmp = this.notAvailableDates.filter(x => new Date(x).getTime() < date && new Date(x).getTime() > new Date(this.range.controls.start.value).getTime());
+    if (max_tmp.length != 0)
+      this.dateDPError("Les dates séléctionnées ne sont pas toutes disponibles");
+    else
+      this.emitDateChange.next(this.range);
   }
 }
