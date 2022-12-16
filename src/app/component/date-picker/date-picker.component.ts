6409b7796488ab08dcb7b22ea52b4c501c6b31ed
Leclercq David <davidleclercq2002@gmail.com>
2022-12-16 13:32:28 +0100

    Refactor endDateChange method

--- src/app/component/date-picker/date-picker.component.ts
+++ src/app/component/date-picker/date-picker.component.ts
@@ -31,7 +31,7 @@ export class DatePickerComponent implements OnInit {
     end: new FormControl<Date | null>(null, Validators.required),
   });
 
-  @Input() notAvailableDates!: Date[] ;
+  @Input() notAvailableDates!: Date[];
 
   dateFilter = (d: Date): boolean => {
     const time = new Date(d).getTime();
@@ -42,28 +42,6 @@ export class DatePickerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
-
-  startDateChange(e: any) { // Changer max pour les dates indispos
-    /*const date_tmp = e.value;
-
-    if (!date_tmp) return;
-
-    const date = new Date(date_tmp).getTime();
-    if (date == this.max.getTime()) {
-      this.dateDPError("Cette date n'est pas disponible");
-      return;
-    }
-
-    if (this.notAvailableDates.length == 0) return;
-
-    let max_tmp = this.notAvailableDates.filter(x => new Date(x).getTime() > date);
-
-    if (max_tmp.length != 0) {
-      max_tmp.sort((a, b) => a.getTime() - b.getTime());
-      //if (date == max_tmp[0].getTime() - (1000 * 60 * 60 * 24))
-        //this.dateDPError("Cette date n'est pas disponible");
-    }*/
   }
 
   private dateDPError(sent: string) {
@@ -77,14 +55,17 @@ export class DatePickerComponent implements OnInit {
 
     if (!date_tmp) return;
 
-    const date = new Date(date_tmp).getTime();
-
-    if (this.notAvailableDates.length == 0) return;
-
     if (this.range.controls.start.value == null) return;
-    // @ts-ignore
-    let max_tmp = this.notAvailableDates.filter(x => new Date(x).getTime() < date && new Date(x).getTime() > new Date(this.range.controls.start.value).getTime());
-    if (max_tmp.length != 0)
+
+    const startDateSelected: number = new Date(this.range.controls.start.value).getTime();
+    const endDateSelected: number = new Date(date_tmp).getTime();
+
+    let conflictsDate = this.notAvailableDates.filter(item =>
+      new Date(item).getTime() < endDateSelected &&
+      new Date(item).getTime() > startDateSelected
+    );
+
+    if (conflictsDate.length !== 0)
       this.dateDPError("Les dates séléctionnées ne sont pas toutes disponibles");
     else
       this.emitDateChange.next(this.range);
